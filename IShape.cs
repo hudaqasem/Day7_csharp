9a5a171f4adae402ccd51148e291f3a44ea90f24
Huda Qasem <114101216+hudaqasem@users.noreply.github.com>
2024-12-14 00:41:56 +0200

    Add files via upload

---
+++ IShape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7.Interfaces
+{
+    internal interface IShape
+    {
+        public double Area { get;}
+        public void Draw();
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Area : {Area}");
+        }
+    }
+}
